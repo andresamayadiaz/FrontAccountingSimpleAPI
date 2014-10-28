c6b81352998cf7acc6a6e67ec202e382667a7ea2
Cambell <cambell.prince@gmail.com>
2014-10-28 22:59:40 +0700

    feature/sales: Added CRUD tests
    - Adding default branch makes the api consistent with the FA ui.
    - This is necessary for testing of the sales api

--- tests/TestEnvironment.php
+++ tests/TestEnvironment.php
@@ -6,18 +6,69 @@ class TestEnvironment
 {
 
 	/**
+	 *
 	 * @return \GuzzleHttp\Client
 	 */
 	public static function client()
 	{
 		return new Client(array(
-				'base_url' => 'http://localhost:8000'
-			), array(
-				'request.options' => array(
-					'exceptions' => false
-				)
+			'base_url' => 'http://localhost:8000'
+		), array(
+			'request.options' => array(
+				'exceptions' => false
 			)
-		);
+		));
+	}
+
+	public static function createCustomer($client, $ref, $name)
+	{
+		$response = $client->post('/modules/api/customers/', array(
+			'headers' => TestEnvironment::headers(),
+			'body' => array(
+				'custname' => $name,
+				'cust_ref' => $ref,
+				'address' => 'address',
+				'tax_id' => 'tax_id',
+				'curr_code' => 'USD',
+				'credit_status' => '1',
+				'payment_terms' => '1',
+				'discount' => '0',
+				'pymt_discount' => '0',
+				'credit_limit' => '1000',
+				'sales_type' => '1',
+				'notes' => 'notes'
+			)
+		));
+		$result = $response->getStatusCode();
+		if ($result != 201) {
+			throw new \Exception('Create customer failed', $result);
+		}
+	}
+
+	public static function createItem($client, $id, $description)
+	{
+		$response = $client->post('/modules/api/inventory/', array(
+			'headers' => TestEnvironment::headers(),
+			'body' => array(
+				'stock_id' => $id,
+				'description' => $description,
+				'long_description' => $description,
+				'category_id' => '1',
+				'tax_type_id' => '1',
+				'units' => 'ea',
+				'mb_flag' => '0',
+				'sales_account' => '1',
+				'inventory_account' => '1',
+				'cogs_account' => '1',
+				'adjustment_account' => '1',
+				'assembly_account' => '1'
+			)
+		));
+
+		$result = $response->getStatusCode();
+		if ($result != 201) {
+			throw new \Exception('Create item failed', $result);
+		}
 	}
 
 	public static function createId()
@@ -27,13 +78,14 @@ class TestEnvironment
 
 	/**
 	 * Return the X-COMPANY, X-USER, and X-PASSWORD headers
+	 *
 	 * @return multitype:string
 	 */
 	public static function headers()
 	{
 		return array(
-			'X-COMPANY'  => '0',
-			'X-USER'     => 'test',
+			'X-COMPANY' => '0',
+			'X-USER' => 'test',
 			'X-PASSWORD' => 'test'
 		);
 	}
@@ -46,8 +98,7 @@ class TestEnvironment
 	private static function resetErrorReporting()
 	{
 		// Undo that which is setup in config.php (of all places)
-		error_reporting(-1);
-
+		error_reporting(- 1);
 	}
 
 	private static function setupAPI()
@@ -68,13 +119,11 @@ class TestEnvironment
 		$Pagehelp = array();
 
 		self::mockRefs();
-
 	}
 
 	private static function setupSQL()
 	{
-		global $db, $show_sql, $sql_trail, $select_trail, $go_debug, $sql_queries, $Ajax,
-			$db_connections, $db_last_inserted_id;
+		global $db, $show_sql, $sql_trail, $select_trail, $go_debug, $sql_queries, $Ajax, $db_connections, $db_last_inserted_id;
 		self::includeFile('config_db.php');
 		self::includeFile('includes/db/connect_db.inc');
 		self::includeFile('includes/db/sql_functions.inc');
@@ -110,7 +159,7 @@ class TestEnvironment
 		global $db_connections;
 		self::setupErrorReporting();
 		self::setupAPI();
-// 		self::resetErrorReporting();
+		// self::resetErrorReporting();
 		self::setupSesstion();
 		self::setupSQL();
 		self::setupSQLDependencies();
@@ -126,26 +175,29 @@ class TestEnvironment
 	public static function includeFile($filePath)
 	{
 		$path_to_root = SRC_PATH;
-		require_once(SRC_PATH . '/' . $filePath);
+		require_once (SRC_PATH . '/' . $filePath);
 	}
 
-	public static function currentAccount() {
+	public static function currentAccount()
+	{
 		return 1;
 	}
 
-	public static function cashAccount() {
+	public static function cashAccount()
+	{
 		return 2;
 	}
 
-	public static function cleanTable($table) {
+	public static function cleanTable($table)
+	{
 		$sql = 'DELETE FROM ' . '0_' . $table;
 		db_query($sql, "Could not clean table '$table'");
 	}
 
-	public static function cleanBanking() {
+	public static function cleanBanking()
+	{
 		self::cleanTable('bank_trans');
 		self::cleanTable('gl_trans');
 		self::cleanTable('comments');
 	}
-
 }
